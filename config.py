820910e2c48e83c379951221818a4bd3ca6f0acb
Ravost99 <75264908+Ravost99@users.noreply.github.com>
2022-01-05 12:52:48 -0600

    Update config.py

--- config.py
+++ config.py
@@ -7,3 +7,12 @@ main_color = 0x4D4D4D
 success = 0x088010
 warning = 0XC1C722
 info = 0x084687
+
+#your bot's guilds
+GUILDS = []
+#your bots token
+TOKEN = "your bot token here"
+#your bots id
+APPLICATION_ID = "your bots id"
+#your bots status optional
+STATUS = ""
